8e9e7efe1aecbd0d2a6cdb193d480a9f347b0fb1
BarrYPL <obartek4@gmail.com>
2022-06-12 17:48:43 +0200

    Added prime sixes

---
+++ prime_sixes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int const num)
+{
+    if (num <= 3)
+    {
+        return num > 1;
+    }
+    else if (num % 2 == 0 || num % 3 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 5; i * i <= num; i += 6)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
+int main()
+{
+    int limit = 0;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> limit;
+
+    for (int i = 2; i <= limit; i++)
+    {
+        if (is_prime(i) && is_prime(i+6))
+        {
+            std::cout << "Liczby pierwsze szóstkowe to: " << i << "," << i+6 << std::endl;
+        }
+    }
+}
